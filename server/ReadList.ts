30449e98edc3bbe88c55f8a74e99ca6d54d14b1f
Gagah Pangeran <git@gagahpangeran.com>
2021-03-25 00:34:58 +0700

    add ReadList class

---
+++ server/ReadList.ts
@@ -0,0 +1,33 @@
+import { Field, ID, ObjectType } from "type-graphql";
+import { v4 as uuidv4 } from "uuid";
+
+@ObjectType()
+export default class ReadList {
+  @Field(type => ID)
+  id: string;
+
+  @Field()
+  link: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  isRead: boolean;
+
+  @Field()
+  createdAt: Date;
+
+  @Field()
+  updatedAt: Date;
+
+  constructor(link: string, title: string, isRead: boolean) {
+    const now = new Date();
+    this.id = uuidv4();
+    this.link = link;
+    this.title = title;
+    this.isRead = isRead;
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+}
