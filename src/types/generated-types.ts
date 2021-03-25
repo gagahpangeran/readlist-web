b2375c0f62b3dda399321578b4a2facb22a36ebb
Gagah Pangeran <git@gagahpangeran.com>
2021-03-25 15:38:35 +0700

    add generated types

---
+++ src/types/generated-types.ts
@@ -0,0 +1,35 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GetReadLists
+// ====================================================
+
+export interface GetReadLists_readLists {
+  __typename: "ReadList";
+  id: string;
+  title: string;
+  link: string;
+  isRead: boolean;
+  submittedAt: DateTime;
+  readAt: DateTime | null;
+}
+
+export interface GetReadLists {
+  readLists: GetReadLists_readLists[];
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
