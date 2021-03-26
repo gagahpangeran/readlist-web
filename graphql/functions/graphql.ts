4be6d7db2eb2b8a3814abb8d588dc4083f9113d0
Gagah Pangeran <git@gagahpangeran.com>
2021-03-26 15:26:15 +0700

    change function handler to support async function

--- graphql/functions/graphql.ts
+++ graphql/functions/graphql.ts
@@ -1,9 +1,27 @@
 import { ApolloServer } from "apollo-server-lambda";
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Callback,
+  Context
+} from "aws-lambda";
 import "reflect-metadata";
-import { buildSchemaSync } from "type-graphql";
+import { buildSchema } from "type-graphql";
 import ReadListResolver from "../ReadListResolver";
 
-const schema = buildSchemaSync({ resolvers: [ReadListResolver] });
-const server = new ApolloServer({ schema });
+const createHandler = async () => {
+  const schema = await buildSchema({ resolvers: [ReadListResolver] });
+  const server = new ApolloServer({ schema });
+  return server.createHandler();
+};
 
-export const handler = server.createHandler();
+export const handler = (
+  event: APIGatewayProxyEvent,
+  context: Context,
+  callback?: Callback<APIGatewayProxyResult>
+) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  createHandler().then(handler => {
+    handler(event, context, callback);
+  });
+};
