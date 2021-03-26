5ddf0a2ae88a46e974acf6c59ff54323c39d5933
Gagah Pangeran <git@gagahpangeran.com>
2021-03-26 20:07:03 +0700

    add typeorm attribute in ReadList

--- graphql/ReadList.ts
+++ graphql/ReadList.ts
@@ -1,21 +1,28 @@
 import { Field, ID, ObjectType } from "type-graphql";
+import { Column, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
 @ObjectType()
+@Entity()
 export default class ReadList {
   @Field(_type => ID)
+  @PrimaryColumn()
   id: string;
 
   @Field()
+  @Column()
   link: string;
 
   @Field()
+  @Column()
   title: string;
 
   @Field()
+  @Column()
   submittedAt: Date;
 
   @Field({ nullable: true })
+  @Column({ nullable: true })
   readAt: Date | null;
 
   constructor(link: string, title: string, isRead: boolean) {
