7e53e4799410264cdc0a6fcdaebcae26b6d19a9e
Gagah Pangeran <git@gagahpangeran.com>
2021-03-28 00:35:17 +0700

    use DeleteButton in ToolbarSelect

--- src/components/Toolbar/ToolbarSelect.tsx
+++ src/components/Toolbar/ToolbarSelect.tsx
@@ -1,15 +1,6 @@
-import { useMutation } from "@apollo/client";
-import IconButton from "@material-ui/core/IconButton";
-import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
-import DeleteIcon from "@material-ui/icons/Delete";
 import React from "react";
-import { DELETE_READ_LISTS } from "../../gql/mutation";
-import { GET_ALL_READ_LISTS } from "../../gql/query";
-import {
-  DeleteReadLists,
-  DeleteReadListsVariables
-} from "../../types/generated-types";
+import DeleteButton from "../Button/DeleteButton";
 
 interface Props {
   className: string;
@@ -18,21 +9,6 @@ interface Props {
 
 export default function ToolbarSelect(props: Props) {
   const { className, selected } = props;
-
-  const [deleteReadLists] = useMutation<
-    DeleteReadLists,
-    DeleteReadListsVariables
-  >(DELETE_READ_LISTS, {
-    refetchQueries: [
-      {
-        query: GET_ALL_READ_LISTS
-      }
-    ]
-  });
-
-  const handleClick = async () => {
-    await deleteReadLists({ variables: { ids: selected } });
-  };
 
   return (
     <>
@@ -45,11 +21,7 @@ export default function ToolbarSelect(props: Props) {
         {selected.length} selected
       </Typography>
 
-      <Tooltip title="Delete">
-        <IconButton aria-label="delete" color="secondary" onClick={handleClick}>
-          <DeleteIcon />
-        </IconButton>
-      </Tooltip>
+      <DeleteButton ids={selected} />
     </>
   );
 }
