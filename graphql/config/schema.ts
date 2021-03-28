c19b102f873be177e4e8c024d8e6c684d0af3c0f
Gagah Pangeran <git@gagahpangeran.com>
2021-03-28 14:19:17 +0700

    delete backend code
    From now this is a separated project

--- graphql/config/schema.ts
+++
@@ -1,6 +0,0 @@
-import { buildSchema } from "type-graphql";
-import ReadListResolver from "../resolver/ReadListResolver";
-
-export async function createSchema() {
-  return await buildSchema({ resolvers: [ReadListResolver] });
-}
--- graphql/migrations/index.ts
+++
@@ -1,5 +0,0 @@
-import { CreateReadListTable1616907307754 } from "./1616907307754-CreateReadListTable";
-
-const migrations = [CreateReadListTable1616907307754];
-
-export default migrations;
