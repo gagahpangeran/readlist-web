8e8bac9d8ec0879c1854a576bf964592495c1812
Gagah Pangeran <git@gagahpangeran.com>
2021-03-30 18:34:34 +0700

    clean up TableHead

--- src/components/Table/TableHead.tsx
+++ src/components/Table/TableHead.tsx
@@ -1,9 +1,7 @@
 import Checkbox from "@material-ui/core/Checkbox";
-import { createStyles, makeStyles } from "@material-ui/core/styles";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
 import React from "react";
 import { Order, ReadListKey } from "../../utils/table";
 
@@ -20,50 +18,8 @@ interface Props {
   showComment: boolean;
 }
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    visuallyHidden: {
-      border: 0,
-      clip: "rect(0 0 0 0)",
-      height: 1,
-      margin: -1,
-      overflow: "hidden",
-      padding: 0,
-      position: "absolute",
-      top: 20,
-      width: 1
-    }
-  })
-);
-
-interface HeadCell {
-  id: ReadListKey;
-  label: string;
-  align: "left" | "right";
-}
-
-const headCells: HeadCell[] = [
-  { id: "title", label: "Title", align: "left" },
-  { id: "readAt", label: "Read At", align: "left" },
-  { id: "comment", label: "Comment", align: "left" }
-];
-
 export default function ReadListTableHead(props: Props) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-    showComment
-  } = props;
-  const createSortHandler = (property: ReadListKey) => (
-    event: React.MouseEvent<unknown>
-  ) => {
-    onRequestSort(event, property);
-  };
-  const classes = useStyles();
+  const { onSelectAllClick, numSelected, rowCount, showComment } = props;
 
   return (
     <TableHead>
@@ -75,31 +31,12 @@ export default function ReadListTableHead(props: Props) {
             onChange={onSelectAllClick}
           />
         </TableCell>
-        {headCells
-          .filter(headCell => headCell.id !== "comment" || showComment)
-          .map(headCell => (
-            <TableCell
-              key={headCell.id}
-              align={headCell.align}
-              sortDirection={orderBy === headCell.id ? order : false}
-            >
-              <TableSortLabel
-                active={orderBy === headCell.id}
-                direction={orderBy === headCell.id ? order : "asc"}
-                onClick={createSortHandler(headCell.id)}
-              >
-                {headCell.label}
-                {orderBy === headCell.id ? (
-                  <span className={classes.visuallyHidden}>
-                    {order === "desc"
-                      ? "sorted descending"
-                      : "sorted ascending"}
-                  </span>
-                ) : null}
-              </TableSortLabel>
-            </TableCell>
-          ))}
-        <TableCell align="center">Actions</TableCell>
+        <TableCell width={showComment ? "460" : "550"}>Title</TableCell>
+        <TableCell width="90">Read At</TableCell>
+        {showComment && <TableCell width="90">Comment</TableCell>}
+        <TableCell align="center" width="100">
+          Actions
+        </TableCell>
       </TableRow>
     </TableHead>
   );
