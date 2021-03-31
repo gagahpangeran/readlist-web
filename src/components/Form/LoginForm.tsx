0c787265cd3002f49b3c015ecd46ff4a711b4f34
Gagah Pangeran <git@gagahpangeran.com>
2021-03-31 23:51:44 +0700

    add useMutation in LoginForm

--- src/components/Form/LoginForm.tsx
+++ src/components/Form/LoginForm.tsx
@@ -1,10 +1,14 @@
+import { useMutation } from "@apollo/client";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { LOGIN } from "../../gql/mutation";
+import { Login, LoginVariables } from "../../types/generated-types";
 
 interface Props {
   open: boolean;
@@ -12,29 +16,62 @@ interface Props {
 }
 
 export default function LoginForm({ open, handleClose }: Props) {
+  const { register, handleSubmit } = useForm<LoginVariables>();
+  const [login, { data, loading, error }] = useMutation<Login, LoginVariables>(
+    LOGIN,
+    { errorPolicy: "all" }
+  );
+
+  const onSubmit = async (inputData: LoginVariables) => {
+    await login({ variables: { ...inputData } });
+  };
+
+  useEffect(() => {
+    if (!loading) {
+      if (data !== undefined) {
+        console.log(data);
+      }
+
+      if (error !== undefined) {
+        console.log(error.message);
+      }
+    }
+  }, [data, loading, error]);
+
   return (
     <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Login</DialogTitle>
-      <DialogContent>
-        <TextField
-          autoFocus
-          margin="dense"
-          label="Username"
-          type="text"
-          fullWidth
-        />
-      </DialogContent>
-      <DialogContent>
-        <TextField margin="dense" label="Password" type="password" fullWidth />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="secondary">
-          Cancel
-        </Button>
-        <Button variant="contained" type="submit" color="primary">
-          Login
-        </Button>
-      </DialogActions>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <TextField
+            inputRef={register}
+            name="username"
+            autoFocus
+            margin="dense"
+            label="Username"
+            type="text"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogContent>
+          <TextField
+            inputRef={register}
+            name="password"
+            margin="dense"
+            label="Password"
+            type="password"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button variant="contained" type="submit" color="primary">
+            Login
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 }
