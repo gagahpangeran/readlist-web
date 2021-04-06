0ae0b487671a8f91882716cc017911cff536e778
Gagah Pangeran <git@gagahpangeran.com>
2021-04-06 22:54:27 +0700

    add custom typeDefs for client query

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,8 +1,15 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, gql as graphql, InMemoryCache } from "@apollo/client";
+
+const typeDefs = graphql`
+  extend type Query {
+    isLogin: Boolean!
+  }
+`;
 
 export const cache = new InMemoryCache();
 
 export const apolloClient = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL,
-  cache
+  cache,
+  typeDefs
 });
