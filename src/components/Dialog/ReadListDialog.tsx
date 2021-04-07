c671abc4617fbda32d343300b48c5305555ae2da
Gagah Pangeran <git@gagahpangeran.com>
2021-04-07 20:42:59 +0700

    add all field read list dialog form

--- src/components/Dialog/ReadListDialog.tsx
+++ src/components/Dialog/ReadListDialog.tsx
@@ -1,8 +1,10 @@
 import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 import TextField from "@material-ui/core/TextField";
 import React from "react";
 import useDialog from "../../gql/dialog";
@@ -11,11 +13,31 @@ export default function ReadListDialog() {
   const { openedDialog, closeDialog } = useDialog();
 
   return (
-    <Dialog open={openedDialog === "readlist"} onClose={closeDialog}>
+    <Dialog open={openedDialog === "readlist"} onClose={closeDialog} fullWidth>
       <DialogTitle>Add New Read List</DialogTitle>
       <DialogContent>
-        <TextField name="link" margin="dense" label="Link" type="text" />
+        <TextField required name="link" label="Link" fullWidth />
       </DialogContent>
+
+      <DialogContent>
+        <TextField required name="title" label="Title" fullWidth />
+      </DialogContent>
+
+      <DialogContent>
+        <FormControlLabel
+          control={<Checkbox defaultChecked name="isRead" color="primary" />}
+          label="Already Read?"
+        />
+      </DialogContent>
+
+      <DialogContent>
+        <TextField type="date" name="readAt" fullWidth />
+      </DialogContent>
+
+      <DialogContent>
+        <TextField name="comment" placeholder="Comment" multiline fullWidth />
+      </DialogContent>
+
       <DialogActions>
         <Button color="secondary" onClick={closeDialog}>
           Cancel
