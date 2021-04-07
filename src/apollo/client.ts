dcdaa6155ec64250d87a8b4bd8d15248b0f4f269
Gagah Pangeran <git@gagahpangeran.com>
2021-04-07 21:15:27 +0700

    add authLink to auth query and mutation

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,8 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
 
 export const cache = new InMemoryCache();
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL,
+  link: authLink.concat(httpLink),
   cache
 });
