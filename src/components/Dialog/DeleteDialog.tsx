9a15c9505417241150c5e0d77a8b1f9724998f98
Gagah Pangeran <git@gagahpangeran.com>
2021-04-09 12:46:36 +0700

    use useReadListDelete in DeleteDialog

--- src/components/Dialog/DeleteDialog.tsx
+++ src/components/Dialog/DeleteDialog.tsx
@@ -1,3 +1,4 @@
+import { LinearProgress } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -5,22 +6,40 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import React from "react";
 import { useDialog } from "../../hooks/dialog";
+import { useDeleteReadList, useReadListSelect } from "../../hooks/readlist";
 
 export default function DeleteDialog() {
   const { openedDialog, closeDialog } = useDialog();
+  const { selected, setSelected } = useReadListSelect();
+
+  const { deleteReadLists, loading } = useDeleteReadList();
+
+  const handleSubmit = async () => {
+    await deleteReadLists({ variables: { ids: selected } });
+    setSelected([]);
+    closeDialog();
+  };
 
   return (
     <Dialog open={openedDialog === "delete"} onClose={closeDialog}>
       <DialogTitle>Delete List</DialogTitle>
-      <DialogContent>Are you sure want to delete this list</DialogContent>
+      <DialogContent>
+        {`Are you sure you want to delete ${selected.length} list(s)`}
+      </DialogContent>
       <DialogActions>
         <Button color="primary" onClick={closeDialog}>
           Cancel
         </Button>
-        <Button variant="contained" type="submit" color="secondary">
+        <Button
+          variant="contained"
+          type="submit"
+          color="secondary"
+          onClick={handleSubmit}
+        >
           Delete
         </Button>
       </DialogActions>
+      {loading && <LinearProgress />}
     </Dialog>
   );
 }
