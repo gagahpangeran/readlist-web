1c323a40bb00d1dba473ff00b02925af927ff5eb
Gagah Pangeran <git@gagahpangeran.com>
2021-04-10 11:23:23 +0700

    add react hooks form to filter toolbar

--- src/components/Toolbar/ToolbarFilter.tsx
+++ src/components/Toolbar/ToolbarFilter.tsx
@@ -8,6 +8,7 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Toolbar from "@material-ui/core/Toolbar";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,76 +21,141 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+interface InputForm {
+  searchBy: "title" | "link";
+  searchKeyword: string;
+  commentStatus: "all" | "with" | "without";
+  readStatus: "all" | "read" | "unread";
+  readFrom: string;
+  readTo: string;
+}
+
 export default function ToolbarFilter() {
   const classes = useStyles();
 
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    reset
+  } = useForm<InputForm>();
+
+  const onSubmit = (inputData: InputForm) => {
+    console.log(inputData);
+  };
+
+  const disableDate = watch("readStatus") === "unread";
+
   return (
     <Toolbar>
-      <Grid container spacing={1}>
-        <Grid item xs={12} sm={3}>
-          <FormControl fullWidth>
-            <InputLabel>Search by</InputLabel>
-            <Select fullWidth>
-              <MenuItem>Title</MenuItem>
-              <MenuItem>Link</MenuItem>
-              <MenuItem>Comment</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField name="keyword" label="Search Keyword" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <FormControl fullWidth>
-            <InputLabel>Comment Status</InputLabel>
-            <Select fullWidth>
-              <MenuItem>All Posts</MenuItem>
-              <MenuItem>Only With Comment</MenuItem>
-              <MenuItem>Only Without Comment</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={2}>
-          <FormControl fullWidth>
-            <InputLabel>Read Status</InputLabel>
-            <Select fullWidth>
-              <MenuItem>All</MenuItem>
-              <MenuItem>Read</MenuItem>
-              <MenuItem>Unread</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={5}>
-          <TextField
-            label="From"
-            type="date"
-            InputLabelProps={{
-              shrink: true
-            }}
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={5}>
-          <TextField
-            label="To"
-            type="date"
-            InputLabelProps={{
-              shrink: true
-            }}
-            fullWidth
-          />
-        </Grid>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={3}>
+            <FormControl fullWidth>
+              <InputLabel>Search by</InputLabel>
+              <Controller
+                control={control}
+                name="searchBy"
+                defaultValue="title"
+                as={
+                  <Select required fullWidth>
+                    <MenuItem value="title">Title</MenuItem>
+                    <MenuItem value="link">Link</MenuItem>
+                  </Select>
+                }
+              />
+            </FormControl>
+          </Grid>
 
-        <Grid item xs={12} className={classes.buttonWrapper}>
-          <Button>Cancel</Button>
-          <Button variant="outlined" color="secondary">
-            Reset
-          </Button>
-          <Button variant="contained" color="primary">
-            Apply
-          </Button>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="searchKeyword"
+              inputRef={register}
+              label="Search Keyword"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={3}>
+            <FormControl fullWidth>
+              <InputLabel>Comment Status</InputLabel>
+              <Controller
+                control={control}
+                name="commentStatus"
+                defaultValue="all"
+                as={
+                  <Select required fullWidth>
+                    <MenuItem value="all">All Posts</MenuItem>
+                    <MenuItem value="with">Only With Comment</MenuItem>
+                    <MenuItem value="without">Only Without Comment</MenuItem>
+                  </Select>
+                }
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={2}>
+            <FormControl fullWidth>
+              <InputLabel>Read Status</InputLabel>
+              <Controller
+                control={control}
+                name="readStatus"
+                defaultValue="read"
+                as={
+                  <Select required fullWidth>
+                    <MenuItem value="all">All</MenuItem>
+                    <MenuItem value="read">Read</MenuItem>
+                    <MenuItem value="unread">Unread</MenuItem>
+                  </Select>
+                }
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={5}>
+            <TextField
+              name="readFrom"
+              inputRef={register}
+              label="From"
+              type="date"
+              InputLabelProps={{
+                shrink: true
+              }}
+              disabled={disableDate}
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={5}>
+            <TextField
+              name="readTo"
+              inputRef={register}
+              label="To"
+              type="date"
+              InputLabelProps={{
+                shrink: true
+              }}
+              disabled={disableDate}
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} className={classes.buttonWrapper}>
+            <Button>Cancel</Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={() => reset()}
+            >
+              Reset
+            </Button>
+            <Button variant="contained" color="primary" type="submit">
+              Apply
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Toolbar>
   );
 }
