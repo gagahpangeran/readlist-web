a5b282168956279c9842ad2e74225039d976a515
Gagah Pangeran <git@gagahpangeran.com>
2021-04-12 19:37:25 +0700

    add dark pallete

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import { ApolloProvider } from "@apollo/client";
 import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import { apolloClient } from "./apollo/client";
 import DialogContainer from "./components/Dialog/DialogContainer";
 import SnackbarContainer from "./components/Snackbar/SnackbarContainer";
 import ReadListTable from "./components/Table/Table";
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
-      <Container component="main" maxWidth="md">
-        <Typography variant="h2">Read List</Typography>
-        <ReadListTable />
-      </Container>
-      <DialogContainer />
-      <SnackbarContainer />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container component="main" maxWidth="md">
+          <ReadListTable />
+        </Container>
+        <DialogContainer />
+        <SnackbarContainer />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
